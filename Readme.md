2da1782e34cf6c40009d64fe07b14d372d339139
Gurudatt Shahane <gurudattshahane50@gmail.com>
2022-08-23 14:59:46 +0530

    Added program and readme file

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Website Blocker for windows
+
+This python code modifies windows file to block some website for all browsers.
+
+To run this code you need python 3 installed in your machine. Apart from that it does not utilize any third party module.
---
+++ program.py
@@ -0,0 +1,30 @@
+import os
+import ctypes, sys
+import subprocess
+
+def main():
+    path = os.path.join("C:\\Windows\\System32\\drivers\\etc","hosts")
+    if os.path.exists(path):
+        with open(path, 'a') as f:
+            f.write("\n127.0.0.1 facebook.com")
+            f.write("\n127.0.0.1 www.facebook.com")
+            f.write("\n127.0.0.1 instagram.com")
+            f.write("\n127.0.0.1 www.instagram.com")
+            f.write("\n127.0.0.1 twitter.com")
+            f.write("\n127.0.0.1 www.twitter.com")
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+# def destruct_self():
+#     subprocess.Popen(['del', sys.argv[0]], shell=True)
+
+if __name__ == '__main__':
+    if is_admin():
+        main()
+        # destruct_self()
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
