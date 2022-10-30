35feb4ce5eb62326dafcd6f194e020110aafd3f2
gurudatt99 <78208341+gurudatt99@users.noreply.github.com>
2022-10-30 10:04:52 +0530

    Update main.py
    Added Changes

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import os
 import ctypes, sys
 import subprocess
 
+# Checks whether the user have admin priviledges or not
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
 
-# def destruct_self():
-#     subprocess.Popen(['del', sys.argv[0]], shell=True)
-
 def main():
     path = os.path.join("C:\\Windows\\System32\\drivers\\etc","hosts")
     if os.path.exists(path):
         with open(path, 'a') as f:
+            # Example websites
             f.write("\n127.0.0.1 facebook.com")
             f.write("\n127.0.0.1 www.facebook.com")
             f.write("\n127.0.0.1 instagram.com")
@@ -25,6 +24,15 @@ def main():
 if __name__ == '__main__':
     if is_admin():
         main()
-        # destruct_self()
     else:
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
+# Note: If you want to revert the changes, then you will have to manually edit C:\\Windows\\System32\\drivers\\etc\\hosts file 
+# and remove added entries.
+# In this case:
+# 127.0.0.1 facebook.com
+# 127.0.0.1 www.facebook.com
+# 127.0.0.1 instagram.com
+# 127.0.0.1 www.instagram.com
+# 127.0.0.1 twitter.com
+# 127.0.0.1 www.twitter.com
